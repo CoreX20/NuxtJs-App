838eac6116b6abdf021dab9db04b95e81ec44d76
kornel <korneliussetiawan13@gmail.com>
2023-08-27 18:40:25 +0700

    tugas 2 revisi

--- components/footerWeb.vue
+++ components/footerWeb.vue
@@ -2,7 +2,9 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
     <div class="container mx-auto flex-column navbar-text">
       <span> &copy; 2023 Kornel S </span>
-      <p>Mahasiswa</p>
+      <p class="mb-0">Mahasiswa</p>
+      <p class="mb-0">Jember, Jawa Timur</p>
+      <p class="mb-0">+628510501664</p>
     </div>
   </nav>
 </template>
